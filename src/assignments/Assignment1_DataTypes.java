825ec99b74b446f0798f01167b6ba5e3c80c1540
Rashmi123-tule <rashmitule1998@gmail.com>
2025-05-24 23:26:36 +0530

    Assignment 1 completed Data Type

---
+++ src/assignments/Assignment1_DataTypes.java
@@ -0,0 +1,76 @@
+package assignments;
+
+public class Assignment1_DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          
+		 double cityTempreature = 25.5;
+		 System.out.println(cityTempreature);
+		 
+		 boolean isCustomerPlacedOrder = true;
+		 boolean notPlacedOrder = false;
+		 System.out.println(isCustomerPlacedOrder);
+		 System.out.println(notPlacedOrder);
+		 
+		 String phoneNumber = "123-456-7890";
+		 System.out.println(phoneNumber);
+		 
+		 double amount = 1000.50;
+		 System.out.println(amount);
+		 
+		 String email = "john.doe@example.com";
+		 System.out.println(email);
+		 
+		 double latitude = 37.7749;
+		 double longitude = -122.4194;
+		 System.out.println(latitude);
+		 System.out.println(longitude);
+		 
+		 boolean married = true;
+		 boolean notmarried = false;
+		 System.out.println(married);
+		 System.out.println(notmarried);
+		 
+		 String occupation = "Software Engineer";
+		 System.out.println(occupation);
+		 
+		 String color = "blue";
+		 System.out.println(color);
+		 
+		 short year = 2023;
+		 System.out.println(year);
+		 
+		 int followers = 1000000;
+		 System.out.println(followers);
+		 
+		 double rating = 7.5;
+		 System.out.println(rating);
+		 
+		 char bloodGroup = 'A';
+		 System.out.println(bloodGroup);
+		 
+		 String bookTitle = "To Kill a Mockingbird";
+		 System.out.println(bookTitle);
+		 
+		 short noEmp = 500;
+		 System.out.println(noEmp);
+		 
+		 String time = "10pm";
+		 System.out.println(time);
+		 
+		 String countryName = "United States";
+		 System.out.println(countryName);
+		 
+		 String eyeColor = "brown";
+		 System.out.println(eyeColor);
+		 
+		 String birthPlace = "new york city";
+		 System.out.println(birthPlace);
+		 
+		 double distance = 200.5;
+		 System.out.println(distance);
+		 
+	}
+
+}
